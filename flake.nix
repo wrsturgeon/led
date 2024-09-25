d49889c8eedbcf66c253c7fdf16bdc73cb140cb9
Will Sturgeon <willstrgn@gmail.com>
2024-09-25 09:56:52 -0700

    Bare-metal one-timer PWM working

--- flake.nix
+++ flake.nix
@@ -33,32 +33,84 @@
           installPhase = "cp -r . $out";
           buildInputs = [ avr-toolchain ];
         };
-        apps.default = {
-          type = "app";
-          program = "${
-            pkgs.stdenvNoCC.mkDerivation {
-              name = "run";
-              src = ./.;
-              buildPhase = ":";
-              installPhase =
-                let
-                  contents = ''
-                    #!/usr/bin/env bash
+        apps =
+          builtins.mapAttrs
+            (_: program: {
+              type = "app";
+              inherit program;
+            })
+            {
+              compile-flags = "${
+                pkgs.stdenvNoCC.mkDerivation {
+                  name = "run";
+                  src = ./.;
+                  buildPhase = ":";
+                  installPhase =
+                    let
+                      filename = "compile_flags.txt";
+                      paths = [
+                        "${avr-toolchain}/lib/gcc/avr/7.3.0/include"
+                        "${avr-toolchain}/lib/gcc/avr/7.3.0/include-fixed"
+                        "${avr-toolchain}/avr/include"
+                      ];
+                      includes = [
+                        "${avr-toolchain}/avr/include/avr/iom4809.h"
+                      ];
+                      contents = ''
+                        #!/usr/bin/env bash
 
-                    set -eux
+                        set -eu
 
-                    make -C ${self.packages.${system}.default} upload
-                  '';
-                in
-                ''
-                  mkdir -p $out/bin
-                  echo '${contents}' > $out/bin/run
-                  chmod +x $out/bin/run
-                '';
-              propagatedBuildInputs = [ avr-toolchain ];
-            }
-          }/bin/run";
-        };
+                        echo
+                        echo > ${filename}
+                        ${pkgs.lib.strings.concatLines (
+                          builtins.map (path: ''
+                            if [ -d ${path} ]; then :; else echo '${path} not found'; echo; fi
+                            ${pkgs.coreutils}/bin/echo '-isystem' >> ${filename}
+                            ${pkgs.coreutils}/bin/echo '${path}' >> ${filename}
+                          '') paths
+                        )}
+                        ${pkgs.lib.strings.concatLines (
+                          builtins.map (include: ''
+                            if [ -f ${include} ]; then :; else echo '${include} not found'; echo; fi
+                            ${pkgs.coreutils}/bin/echo '-include' >> ${filename}
+                            ${pkgs.coreutils}/bin/echo '${include}' >> ${filename}
+                          '') includes
+                        )}
+                      '';
+                    in
+                    ''
+                      mkdir -p $out/bin
+                      echo '${contents}' > $out/bin/run
+                      chmod +x $out/bin/run
+                    '';
+                  propagatedBuildInputs = [ avr-toolchain ];
+                }
+              }/bin/run";
+              default = "${
+                pkgs.stdenvNoCC.mkDerivation {
+                  name = "run";
+                  src = ./.;
+                  buildPhase = ":";
+                  installPhase =
+                    let
+                      contents = ''
+                        #!/usr/bin/env bash
+
+                        set -eux
+
+                        make -C ${self.packages.${system}.default} upload
+                      '';
+                    in
+                    ''
+                      mkdir -p $out/bin
+                      echo '${contents}' > $out/bin/run
+                      chmod +x $out/bin/run
+                    '';
+                  propagatedBuildInputs = [ avr-toolchain ];
+                }
+              }/bin/run";
+            };
       }
     );
 }
--- main.c
+++ main.c
@@ -1,129 +1,258 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% READ THIS FIRST:
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// The signal for servo number [N] is sent through pin D[N].
+// For example, servo number 3 should get its signal from pin D3,
+// which is labelled PD3 in the pinout below:
+// https://docs.arduino.cc/resources/pinouts/ABX00028-full-pinout.pdf
+
+// If you want to edit settings like beats per minute or servo range,
+// look past the short import section (which has to stay at the top)
+// to the next section, labelled "SETTINGS:"
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Import some information about the board:
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
 #include <math.h>
 #include <stdint.h>
 
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SETTINGS:
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// Beats per minute:
+#define BPM 45
+
+// How many servos are we using?
+#define N_SERVOS 3
+
+// Minimum pulse percent (/100):
+#define MIN_PULSE_PERCENT 20
+
+// Maximum pulse percent (/100):
+#define MAX_PULSE_PERCENT 70
+
+// How far into the last servo's cycle should the next servo's cycle start?
+#define PULSE_STAGGER 0.075
+
+// How sharp should the heart's movements be?
+// (i.e. delineated "beats" versus smooth oscillation)
+#define PULSE_STRENGTH 6
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Below this line is complex: tread carefully!
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#if N_SERVOS > 8
+#error Only 8 servos (or fewer) are supported, since there are only 8 pins in port D
+#endif
+
+#if MIN_PULSE_PERCENT < 0
+#error Minimum pulse percentage is less than 0
+#endif
+
+#if MAX_PULSE_PERCENT < 0
+#error Maximum pulse percentage is less than 0
+#endif
+
+#if MIN_PULSE_PERCENT > 100
+#error Minimum pulse percentage is more than 100
+#endif
+
+#if MAX_PULSE_PERCENT > 100
+#error Maximum pulse percentage is more than 100
+#endif
+
+#if MAX_PULSE_PERCENT < MIN_PULSE_PERCENT
+#error Maximum pulse percent is less than minimum pulse percent
+#endif
+
+#if PULSE_STRENGTH > 10
+#error `PULSE_STRENGTH` is way too high! Each +1 in `PULSE_STRENGTH` doubles actual strength!
+#endif
+
+// Convert beats per minute to a heartbeat period in terms of servo pulses:
+#define PERIOD_CYCLES ((60ULL * 50ULL) / (unsigned long long)(BPM))
+
 /* CALCULATIONS:
- * - On p. 200 of <https://ww1.microchip.com/downloads/aemDocuments/documents/MCU08/ProductDocuments/DataSheets/ATmega4808-09-DataSheet-DS40002173C.pdf>:
- *   - Dual-slope PWM frequency = f_CLK_PER / (2N * PER) where
+ * - On p. 200 of
+ * <https://ww1.microchip.com/downloads/aemDocuments/documents/MCU08/ProductDocuments/DataSheets/ATmega4808-09-DataSheet-DS40002173C.pdf>:
+ *   - Single-slope PWM frequency = f_CLK_PER / N(PER + 1) where
  *     - N represents the prescaler divider
- *     - PER (presumably) represents the period variable w/ a minimum resolution of 2 bits (i.e. 0x3)
- *     - f_CLK_PER is 16MHz prescaled by 6 = 16MHz / 6 = 2.666...Hz ~= 2,666,666Hz
- *   - So, under the conditions described on the first line, this should be 2,666,666Hz / (2 * 8 * 65535) ~= 2.54317Hz
- *   - We want a frequency of 50Hz for a period of 20ms
- *     - 2,666,666Hz / (2 * PRS * PER) = 50Hz
- *     - 2,666,666Hz / (PRS * PER) = 100Hz
- *     - 2,666,666Hz / 100Hz = PRS * PER
- *     - 26,666.66 = PRS * PER
- *     - PRS = 1, PER = 26,666.66 ~= 26,667
+ *     - PER (presumably) represents the period variable
+ *       w/ a minimum resolution of 2 bits (i.e. 0x3)
+ *     - f_CLK_PER is 16MHz prescaled by 6:
+ *       16MHz / 6 = 2.666...Hz ~= 2,666,666Hz
+ *     - I'm guessing that the "+ 1" means that
+ *       the clock goes up to *and including* `PER`
+ *   - We want the period to be 20ms (i.e. we want the frequency to be 50Hz):
+ *     - f_CLK_PER / N(PER + 1) = 50Hz
+ *     - 2,666,666Hz / N(PER + 1) = 50Hz
+ *     - 2,666,666 / 50 = N(PER + 1)
+ *     - 2,666,666 / 50 = N(PER + 1)
+ *     - 53,333.3... = N(PER + 1)
+ *   - We want `PER` to be as large as possible, so
+ *     if we pin `PER` to its max value of 0xFFFF,
+ *     we should be just above 53,333.3... total.
+ *     - 53,333.3... <= N(0xFFFF + 1)
+ *     - 53,333.3... <= 65,536N
+ *     - 0.813802... <= N
+ *   - Okay, so that's not possible, but N=1 is close enough
+ *   - Now let's tackle `PER`:
+ *     - 53,333.3... = PER + 1
+ *     - 53,332.3... = PER
+ *     - 53,332 ~= PER
  */
-
-#define PERIOD      (26667ULL)
-#define MILLISECOND ((PERIOD + 10ULL) / 20ULL)
-#define MIN_PULSE ((uint32_t)(1.1F * MILLISECOND))
-#define MAX_PULSE ((uint32_t)(1.8F * MILLISECOND))
+#define PERIOD (53332ULL)                      // 20ms ~= 53,332
+#define MILLISECOND ((PERIOD + 10ULL) / 20ULL) //  1ms ~=  2,667
+#define MIN_PULSE                                                              \
+  ((MILLISECOND * (unsigned long long)(MIN_PULSE_PERCENT)) / 100ULL)
+#define MAX_PULSE                                                              \
+  ((MILLISECOND * (unsigned long long)(MAX_PULSE_PERCENT)) / 100ULL)
 #define PULSE_RANGE ((uint32_t)(MAX_PULSE - MIN_PULSE))
-// #define MIN_PULSE 0
-// #define PULSE_RANGE PERIOD
-#define TIME_TO_OPEN (50000ULL)
 
-static volatile uint32_t counter;
+#define FLOAT_PI (3.14159265359F)
 
-static uint8_t last_counter = 0;
-static _Bool open;
-static float f;
-static uint16_t duty;
+/*
+ * Add an extra 1/8 of a millisecond before each pulse
+ * (even with 8 servos, we'd have 8(1/8ms + 2ms) = 17ms,
+ * so still worst-case 3ms left for calculations, which is plenty):
+ */
+// #define BETWEEN_PULSES (MILLISECOND >> 3ULL)
+#define BETWEEN_PULSES ((MILLISECOND << 1ULL) + (MILLISECOND >> 3ULL))
 
-ISR(TCB0_INT_vect) {
-  ++counter;
-}
-
+// Subroutine to initialize the clock:
 inline static void TCA0_init(void) {
-  PORTMUX.TCAROUTEA // <-- PWM signal to which port (set of pins)?
-    = PORTMUX_TCA0_PORTA_gc // e.g. if `PORT_LTR` is A, then Port A (pins PA0, PA1, etc.)
-    ;
-  TCA0.SINGLE.CTRLB // <-- One set of configuration options for timer TCA
-    = TCA_SINGLE_CMP0EN_bm // Enable comparison channel 0
-    | TCA_SINGLE_WGMODE_DSBOTTOM_gc // Dual-slope mode
-    ;
-  TCA0.SINGLE.EVCTRL // <-- Another set of options about event reactions
-    &= ~TCA_SINGLE_CNTEI_bm // Don't increment a counter each event
-    ;
-  TCA0.SINGLE.PERBUF // <-- Pulse period
-    = PERIOD // Macro defined above
-    ;
-  TCA0.SINGLE.CMP0BUF // <-- Pulse duty cycle
-    = MIN_PULSE; // Macro defined above
-  TCA0.SINGLE.CTRLA // <-- Prescaler & on/off switch
-    = TCA_SINGLE_CLKSEL_DIV1_gc // Run this at 1/4 the clock frequency (4x its cycle)
-    | TCA_SINGLE_ENABLE_bm // Turn it on!
-    ;
+
+  // Set the timer's period to 50ms:
+  TCA0.SINGLE.PERBUF = PERIOD;
+
+  // Run at the usual clock frequency (disable the prescaler):
+  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc;
+
+  // Turn it on!
+  TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
 }
 
-inline static void PORT_init(void) {    
-  PORTA.DIR // <-- Which pin in this port gets the PWM signal?
-    |= PIN0_bm // Route to pin PA0 (not PA1, PA2, etc.)
-    ;
+// Subroutine to enable output pins:
+inline static void PORT_init(void) {
+  for (uint8_t i = 0; i < N_SERVOS; ++i) {
+    PORTD.DIR |= (1U << i); // Enable pin #[i] by setting is DIRection to OUT
+  }
 }
 
-inline static void TCB0_init(void) {
-  // p. 242 under 21.3.2 "Initialization"
-  TCB0.CCMP = 0xFFFF;
-  TCB0.INTCTRL |= 0b1;
-  TCB0.CTRLA |= 0b1;
-}
+inline static uint16_t clock(void) { return TCA0.SINGLE.CNT; }
 
-inline static void input_init(void) {
-  // Pin PD5
-  PORTD.DIRCLR |= (1 << 5); // Disable output on PD5 (make it input-only)
-  PORTD.PIN5CTRL |= (1 << 3); // Enable the pull-up resistor on PD5
+// All variables:
+static uint8_t cycle_count, pin_mask;
+static uint16_t pulse_center, actual_width, moment;
+static uint16_t extra_clocks[N_SERVOS];
+static float angle;
+
+static void update_positions(void) {
+  angle = (cycle_count * (FLOAT_PI / PERIOD_CYCLES)); // [0, pi)
+  for (uint8_t i = 0; i < N_SERVOS; ++i) {
+
+    // Map angle to the first half of `sin` (0-pi, the nonnegative half, only):
+    float pos = sinf(angle); // [0, 1] (since we use only half the cycle)
+
+    // Take `pos` to the power of a power of two
+    // by repeatedly multiplying it with itself:
+    for (uint8_t i = 0; i < PULSE_STRENGTH; ++i) {
+      pos *= pos;
+    }
+
+    // Map to the full range and implicitly convert to an integer:
+    extra_clocks[i] = PULSE_RANGE * pos;
+
+    // Stagger the angle for the next servo:
+    angle += (FLOAT_PI * PULSE_STAGGER);
+  }
 }
 
 int main(void) {
-  counter = 0;
-  open = 0;
-  last_counter = 0;
 
-  cli(); // Disable interrupts (temporarily)
-  PORT_init(); // Select our pin
+  // Initialize the board:
+  cli();       // Disable interrupts (temporarily)
+  PORT_init(); // Enable pins
   TCA0_init(); // Turn PWM on
-  TCB0_init(); // Enable our slower timer
-  input_init();
-  sei(); // Enable interrupts
-  // do {} while (FUSE.OSCCFG & (1 << 7)); // so FUSE.OSCCFG bit 7 is 0, which means that the system oscillator can be modified at runtime
-  // do {} while (!(FUSE.OSCCFG & (1 << 0))); // so FUSE.OSCCFG bit 0 is 1, which means that the system oscillator runs at 16MHz
-  // do {} while (FUSE.OSCCFG & (1 << 1)); // so FUSE.OSCCFG bit 1 is 0, which means that the system oscillator is correctly configured to run at 16MHz
-  // do {} while (CLKCTRL.MCLKCTRLA & (1 << 0)); // so CLKCTRL.MCLKCTRLA bit 0 is 0, which needs the below...
-  // do {} while (CLKCTRL.MCLKCTRLA & (1 << 1)); // so CLKCTRL.MCLKCTRLA bit 1 is 0, which means that CLK_MAIN comes from OSC20M at 16MHz
-  // do {} while (!(CLKCTRL.MCLKCTRLB & (1 << 0))); // so CLKCTRL.MCLKCTRLB bit 0 is 1, which means the CLK_PER prescaler is enabled
-  // do {} while (CLKCTRL.MCLKCTRLB & (1 << 1)); // so CLKCTRL.MCLKCTRLB bit 1 is 0
-  // do {} while (CLKCTRL.MCLKCTRLB & (1 << 2)); // so CLKCTRL.MCLKCTRLB bit 2 is 0
-  // do {} while (CLKCTRL.MCLKCTRLB & (1 << 3)); // so CLKCTRL.MCLKCTRLB bit 3 is 0
-  // do {} while (!(CLKCTRL.MCLKCTRLB & (1 << 4))); // so CLKCTRL.MCLKCTRLB bit 4 is 1, which means CLK_PER is prescaled over 6 (p. 95)
+  sei();       // Enable interrupts
+
+  // Reset the cycle count:
+  cycle_count = 0;
+
+  // Set servo positions for the first time:
+  update_positions();
+
+  // Nothing else to do, so timing can use busy-waiting for readability:
   do {
 
-    do {} while (PORTD.IN & (1 << 5));
-    counter = last_counter = 0;
-    open = !open;
+    // Reset our stopwatch:
+    pulse_center = (uint16_t)(-MILLISECOND);
+    // and subtract a millisecond to account for
+    // the extra millisecond in `BETWEEN_PULSES`
 
+    // Loop over each servo, all in under 20ms total:
+    for (uint8_t i = 0; i < N_SERVOS; ++i) {
+
+      // Add 2ms plus a buffer for computation, interrupts, whatnot:
+      pulse_center += BETWEEN_PULSES;
+
+      // Figure out which pin to power:
+      pin_mask = (1U << i);
+
+      // Now, figure out the actual pulse width in clock cycles (cont'd):
+
+      // First, one millisecond is the absolute minimum,
+      // plus the user-defined minimum on top of that:
+      actual_width = (MILLISECOND + MIN_PULSE);
+
+      // Then the range-adjusted position:
+      actual_width += (extra_clocks[i]);
+
+      // Now, we have to figure out where to start the pulse,
+      // which should be *half* the pulse width *before* the center:
+      moment = (pulse_center - (actual_width >> 1));
+
+      // Wait until that moment:
+      do {
+      } while (clock() < moment);
+
+      // Send power to the corresponding pin:
+      PORTD.OUT |= pin_mask;
+
+      // Figure out when to end the pulse (easy, since we know the width):
+      moment += actual_width;
+
+      // Invert the pin mask (so we're turning it off instead of turning it on):
+      pin_mask = (~pin_mask);
+
+      // Wait until that moment:
+      do {
+      } while (clock() < moment);
+
+      // Cut power from the corresponding pin:
+      PORTD.OUT &= pin_mask;
+
+      // Move on to the next servo (back up top):
+    }
+
+    // Update cycle count & reset if we've finished a set:
+    if (PERIOD_CYCLES == ++cycle_count) {
+      cycle_count = 0;
+    }
+
+    // Update servo positions:
+    update_positions();
+
+    // Wait for the next cycle:
     do {
-      do {} while (last_counter == (uint8_t)counter);
-      last_counter = counter;
-      f = counter;
-      f *= (3.14159265358979323846F / (float)TIME_TO_OPEN);
-      f = cosf(f);
-      f += 1.F;
-      f *= (0.5F * PULSE_RANGE);
-      duty = f;
-      if (open) {
-        duty = -duty;
-        duty += (MIN_PULSE + PULSE_RANGE);
-      } else {
-        duty += MIN_PULSE;
-      }
-      TCA0.SINGLE.CMP0BUF = duty;
-    } while (counter < TIME_TO_OPEN);
+    } while (clock() > moment);
 
   } while (1);
 }
