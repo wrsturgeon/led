28e329d610fb8bda029ac0419fb732225db93ca2
Will Sturgeon <willstrgn@gmail.com>
2024-10-01 17:32:28 -0700

    Working infinite-LED program

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% READ THIS FIRST:
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-// The signal for servo number [N] is sent through pin D[N].
-// For example, servo number 3 should get its signal from pin D3,
+// The signal for LED number [N] is sent through pin D[N].
+// For example, LED number 3 should get its signal from pin D3,
 // which is labelled PD3 in the pinout below:
 // https://docs.arduino.cc/resources/pinouts/ABX00028-full-pinout.pdf
 
-// If you'd like to edit settings like beats per minute or servo range,
+// If you'd like to edit settings like beats per minute or luminosity range,
 // look past the short import section (which has to stay at the top)
 // to the next section, labelled "SETTINGS."
 
@@ -21,65 +21,48 @@
 #include <avr/io.h>
 
 #include <math.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SETTINGS:
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+// Frame rate:
+#define FRAME_RATE 100
+
 // Beats per minute:
 #define BPM 45
 
-// How many servos are we using?
-#define N_SERVOS 3
+// Which pins should send a signal to which LEDs, in order?
+static char const *const PIN_NAMES[] = {"D0", "D1", "D2", "D3"};
 
-// Minimum pulse percent (/100):
-#define MIN_PULSE_PERCENT 0
+// Minimum luminosity percent (/100):
+#define MIN_LUMINOSITY_PERCENT 10
 
-// Maximum pulse percent (/100):
-#define MAX_PULSE_PERCENT 50
+// Maximum luminosity percent (/100):
+#define MAX_LUMINOSITY_PERCENT 90
 
-// How far into the last servo's cycle should the next servo's cycle start?
-#define PULSE_STAGGER 0.075
-
-// How sharp should the heart's movements be?
-// (i.e. delineated "beats" versus smooth oscillation)
-#define PULSE_STRENGTH 5
+// Calculate an LED's position based on its time within a cycle (from 0 to 1)
+// and its ID. NOTE: Make sure the output is between 0 and 1!
+float luminosity(float const percent_of_cycle, uint8_t const id) {
+  return 0.5F * (1.F + sinf(6.2831853072 * percent_of_cycle + id));
+}
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Below this line is complex: tread carefully!
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-#if N_SERVOS > 8
-#error Only 8 servos (or fewer) are supported, since there are only 8 pins in port D
+// Luminosity sanity-checks:
+#if MIN_LUMINOSITY_PERCENT < 0
+#error Minimum luminosity is less than 0%
 #endif
-
-#if MIN_PULSE_PERCENT < 0
-#error Minimum pulse percentage is less than 0
+#if MAX_LUMINOSITY_PERCENT > 100
+#error Maximum luminosity is more than 100%
 #endif
-
-#if MAX_PULSE_PERCENT < 0
-#error Maximum pulse percentage is less than 0
+#if MIN_LUMINOSITY_PERCENT > MAX_LUMINOSITY_PERCENT
+#error Minimum luminosity is more than maximum luminosity
 #endif
-
-#if MIN_PULSE_PERCENT > 100
-#error Minimum pulse percentage is more than 100
-#endif
-
-#if MAX_PULSE_PERCENT > 100
-#error Maximum pulse percentage is more than 100
-#endif
-
-#if MAX_PULSE_PERCENT < MIN_PULSE_PERCENT
-#error Maximum pulse percent is less than minimum pulse percent
-#endif
-
-#if PULSE_STRENGTH > 10
-#error `PULSE_STRENGTH` is way too high! Each +1 in `PULSE_STRENGTH` doubles actual strength!
-#endif
-
-// Convert beats per minute to a heartbeat period in terms of servo pulses:
-#define PERIOD_CYCLES ((60ULL * 50ULL) / (unsigned long long)(BPM))
 
 /* CALCULATIONS:
  * - On p. 200 of
@@ -91,40 +74,157 @@
  *     - f_CLK_PER is 16MHz prescaled by 6:
  *       16MHz / 6 = 2.666...Hz ~= 2,666,666Hz
  *     - Note that "+ 1" means that the clock goes up to *and including* `PER`.
- *   - We want the period to be 20ms (i.e. we want the frequency to be 50Hz):
- *     - f_CLK_PER / N(PER + 1) = 50Hz
- *     - 2,666,666Hz / N(PER + 1) = 50Hz
- *     - 2,666,666 / 50 = N(PER + 1)
- *     - 2,666,666 / 50 = N(PER + 1)
- *     - 53,333.3... = N(PER + 1)
- *   - We want `PER` to be as large as possible, so
- *     if we pin `PER` to its max value of 0xFFFF,
- *     we should be just above 53,333.3... total.
- *     - 53,333.3... <= N(0xFFFF + 1)
- *     - 53,333.3... <= 65,536N
- *     - 0.813802... <= N
- *   - Okay, so that's not possible, but N=1 is close enough
- *   - Now we can calculate `PER`:
- *     - 53,333.3... = PER + 1
- *     - 53,332.3... = PER
- *     - 53,332 ~= PER
+ *   - We can work backwards to yield a specified frame rate:
+ *     - f_CLK_PER / PRESCALE(PERIOD + 1) = FRAME_RATE
+ *     - 2,666,666Hz / PRESCALE(PERIOD + 1) = FRAME_RATE
+ *     - 2,666,666 / FRAME_RATE = PRESCALE(PERIOD + 1)
+ *   - In any sane LED-dimming program, the frame rate will be at least 50Hz,
+ *     and we want `PERIOD` to be as large as possible,
+ *     so let's work backward again:
+ *     - 2,666,666/50 <= PRESCALE * (0xFFFF + 1)
+ *     - 53,333.33... <= PRESCALE * (0xFFFF + 1)
+ *     - 53,333.33... <= PRESCALE * 65,536
+ *     - 0.8138021... <= PRESCALE
+ *   - Okay, so PRESCALE=1 (i.e. no prescaling) is the closest we can get
+ *   - Now we can calculate `PERIOD`:
+ *     - 2,666,666 / FRAME_RATE     = (PERIOD + 1) * PRESCALE
+ *     - 2,666,666 / FRAME_RATE     = (PERIOD + 1) * 1
+ *     - 2,666,666 / FRAME_RATE     = (PERIOD + 1)
+ *     - 2,666,666 / FRAME_RATE - 1 =  PERIOD
  */
-#define PERIOD (53332ULL)                      // 20ms ~= 53,332
-#define MILLISECOND ((PERIOD + 10ULL) / 20ULL) //  1ms ~=  2,667
-#define MIN_PULSE                                                              \
-  ((MILLISECOND * (unsigned long long)(MIN_PULSE_PERCENT)) / 100ULL)
-#define MAX_PULSE                                                              \
-  ((MILLISECOND * (unsigned long long)(MAX_PULSE_PERCENT)) / 100ULL)
-#define PULSE_RANGE ((uint32_t)(MAX_PULSE - MIN_PULSE))
+#define CLOCKS_PER_FRAME (2666666ULL / FRAME_RATE)
+#define PERIOD (CLOCKS_PER_FRAME - 1ULL)
+#if PERIOD > 65535
+#error Frame rate is too slow!
+#endif
+#define HALF_PERIOD (PERIOD >> 1U)
 
-#define FLOAT_PI (3.14159265359F)
+#define PLUS_MINUS_MIN                                                         \
+  (((MIN_LUMINOSITY_PERCENT * HALF_PERIOD) + 50ULL) / 100ULL)
+#define PLUS_MINUS_MAX                                                         \
+  (((MAX_LUMINOSITY_PERCENT * HALF_PERIOD) + 50ULL) / 100ULL)
+#define PLUS_MINUS_RANGE (PLUS_MINUS_MAX - PLUS_MINUS_MIN)
 
-/*
- * Add an extra 1/8 of a millisecond before each pulse
- * (even with 8 servos, we'd have 8(1/8ms + 2ms) = 17ms,
- * so still worst-case 3ms left for calculations, which is plenty):
- */
-#define BETWEEN_PULSES ((MILLISECOND << 1ULL) + (MILLISECOND >> 3ULL))
+// How many LEDs are we using?
+// static uint8_t const N_LEDS = (sizeof(PIN_NAMES) / sizeof(char const
+// *const));
+#define N_LEDS (sizeof(PIN_NAMES) / sizeof(char const *const))
+
+// Convert beats per minute to a heartbeat period in terms of PWM pulses:
+#define PERIOD_IN_CYCLES ((60ULL * FRAME_RATE) / BPM)
+
+typedef struct led {
+  uint16_t pulse_width;
+  uint8_t id;
+} led;
+
+static uint16_t cycle_count;
+static led pulse_widths[N_LEDS];
+
+// Bubble sort is not only fine but actually the most efficient here
+// because (1) it doesn't allocate any memory and (2) servo positions
+// move very slightly if at all between frames, so sorting is usually
+// very close to a no-op, in which case one pass is the fastest.
+static void sort_pulse_widths(void) {
+  uint8_t i;
+  bool changed;
+  do {
+    i = (N_LEDS - 1);
+    changed = 0;
+    do {
+      if (pulse_widths[i].pulse_width < pulse_widths[i - 1].pulse_width) {
+        changed = 1;
+        led const tmp = pulse_widths[i];
+        pulse_widths[i] = pulse_widths[i - 1];
+        pulse_widths[i - 1] = tmp;
+      }
+    } while (--i); // this intentionally omits (i = 0)
+  } while (changed);
+
+  // Check that this worked:
+  /*
+  do {
+    if (pulse_widths[i].pulse_width > pulse_widths[i + 1].pulse_width) {
+      // stop the program:
+      do {
+      } while (1);
+    }
+  } while ((++i) < (N_LEDS - 1));
+  */
+}
+
+static void recalculate_pulse_widths(void) {
+  float const percent_of_cycle = (((float)cycle_count) / PERIOD_IN_CYCLES);
+  for (uint8_t i = 0; i < N_LEDS; ++i) {
+    pulse_widths[i].pulse_width =
+        (PLUS_MINUS_MIN +
+         (uint16_t)(PLUS_MINUS_RANGE *
+                    luminosity(percent_of_cycle, pulse_widths[i].id)));
+  }
+  sort_pulse_widths();
+}
+
+static void init_pulse_widths(void) {
+  for (uint8_t id = 0; id < N_LEDS; ++id) {
+    pulse_widths[id].id = id;
+  }
+  recalculate_pulse_widths();
+}
+
+typedef struct pin {
+  register8_t *unmasked;
+  uint8_t on_mask;
+  uint8_t off_mask;
+} pin;
+
+inline static void pin_on(pin const *const p) {
+  (*(p->unmasked)) |= (p->on_mask);
+}
+inline static void pin_off(pin const *const p) {
+  (*(p->unmasked)) &= (p->off_mask);
+}
+
+static pin PINS[N_LEDS];
+
+static void init_pins(void) {
+  for (uint8_t id = 0; id < N_LEDS; ++id) {
+    uint8_t const digit = (PIN_NAMES[id][1] - '0');
+    uint8_t const mask = (1U << digit);
+    PINS[id].on_mask = mask;
+    PINS[id].off_mask = ~mask;
+
+    switch (PIN_NAMES[id][0]) {
+    case 'A':
+      PORTA.DIR |= mask;
+      PINS[id].unmasked = &PORTA.OUT;
+      break;
+    case 'B':
+      PORTB.DIR |= mask;
+      PINS[id].unmasked = &PORTB.OUT;
+      break;
+    case 'C':
+      PORTC.DIR |= mask;
+      PINS[id].unmasked = &PORTC.OUT;
+      break;
+    case 'D':
+      PORTD.DIR |= mask;
+      PINS[id].unmasked = &PORTD.OUT;
+      break;
+    case 'E':
+      PORTE.DIR |= mask;
+      PINS[id].unmasked = &PORTE.OUT;
+      break;
+    case 'F':
+      PORTF.DIR |= mask;
+      PINS[id].unmasked = &PORTF.OUT;
+      break;
+    default:
+      break;
+    }
+
+    pin_off(&(PINS[id]));
+  }
+}
 
 // Subroutine to initialize the clock:
 inline static void TCA0_init(void) {
@@ -139,123 +239,64 @@ inline static void TCA0_init(void) {
   TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
 }
 
-// Subroutine to enable output pins:
-inline static void PORT_init(void) {
-  for (uint8_t i = 0; i < N_SERVOS; ++i) {
-    PORTD.DIR |= (1U << i); // Enable pin #[i] by setting is DIRection to OUT
-  }
-}
-
 // A specific two-byte region on the microcontroller holds the timer's count:
 inline static uint16_t clock(void) { return TCA0.SINGLE.CNT; }
 
-// Define variables before we use them to save stack instructions:
-static uint8_t cycle_count, pin_mask;
-static uint16_t pulse_center, actual_width, moment;
-static uint16_t extra_clocks[N_SERVOS];
-static float angle;
-
-// This is going to be used twice, so we can save memory and
-// make sure it's the same behavior with a function:
-static void update_positions(void) {
-  angle = (cycle_count * (FLOAT_PI / PERIOD_CYCLES)); // [0, pi)
-  for (uint8_t i = 0; i < N_SERVOS; ++i) {
-
-    // Map angle to the first half of `sin` (0-pi, the nonnegative half, only):
-    float pos = sinf(angle); // [0, 1] (since we use only half the cycle)
-
-    // Take `pos` to the power of a power of two
-    // by repeatedly multiplying it with itself:
-    for (uint8_t i = 0; i < PULSE_STRENGTH; ++i) {
-      pos *= pos;
-    }
-
-    // Map to the full range and implicitly convert to an integer:
-    extra_clocks[i] = PULSE_RANGE * pos;
-
-    // Stagger the angle for the next servo:
-    angle += (FLOAT_PI * PULSE_STAGGER);
-  }
-}
-
 int main(void) {
-
-  // Initialize the board:
-  cli();       // Disable interrupts (temporarily)
-  PORT_init(); // Enable pins
-  TCA0_init(); // Turn PWM on
-  sei();       // Enable interrupts
 
   // Reset the cycle count:
   cycle_count = 0;
 
-  // Set servo positions for the first time:
-  update_positions();
+  // Initialize pulse-width array:
+  init_pulse_widths();
+
+  // Disable interrupts (temporarily):
+  cli();
+
+  // Initialize output pins:
+  init_pins();
+
+  // Turn PWM on:
+  TCA0_init();
+
+  // Enable interrupts
+  sei();
+
+  uint8_t i = (N_LEDS - 1);
+  uint16_t moment;
 
   // Nothing else to do, so timing can use busy-waiting for readability:
   do {
 
-    // Reset our stopwatch:
-    pulse_center = (uint16_t)(-MILLISECOND);
-    // and subtract a millisecond to account for
-    // the extra millisecond in `BETWEEN_PULSES`
+    // Start with the longest pulse width (i.e. keep `i` at `N_LEDS - 1`),
+    // since it will start first:
+    do {
+      moment = (HALF_PERIOD - (pulse_widths[i].pulse_width));
+      do { // nothing
+      } while (TCA0.SINGLE.CNT < moment);
+      pin_on(&(PINS[pulse_widths[i].id]));
+    } while (i--); // down to, and including, (i = 0)
 
-    // Loop over each servo, all in under 20ms total:
-    for (uint8_t i = 0; i < N_SERVOS; ++i) {
-
-      // Add 2ms plus a buffer for computation, interrupts, whatnot:
-      pulse_center += BETWEEN_PULSES;
-
-      // Figure out which pin to power:
-      pin_mask = (1U << i);
-
-      // Now, figure out the actual pulse width in clock cycles (cont'd):
-
-      // First, one millisecond is the absolute minimum,
-      // plus the user-defined minimum on top of that:
-      actual_width = (MILLISECOND + MIN_PULSE);
-
-      // Then the range-adjusted position:
-      actual_width += (extra_clocks[i]);
-
-      // Now, we have to figure out where to start the pulse,
-      // which should be *half* the pulse width *before* the center:
-      moment = (pulse_center - (actual_width >> 1));
-
-      // Wait until that moment:
-      do {
-      } while (clock() < moment);
-
-      // Send power to the corresponding pin:
-      PORTD.OUT |= pin_mask;
-
-      // Figure out when to end the pulse (easy, since we know the width):
-      moment += actual_width;
-
-      // Invert the pin mask (so we're turning it off instead of turning it on):
-      pin_mask = (~pin_mask);
-
-      // Wait until that moment:
-      do {
-      } while (clock() < moment);
-
-      // Cut power from the corresponding pin:
-      PORTD.OUT &= pin_mask;
-
-      // Move on to the next servo (back up top):
-    }
+    // Then start with the shortest pulse width (i.e. keep `i` at 0),
+    // since it will end first:
+    do {
+      moment = (HALF_PERIOD + (pulse_widths[i].pulse_width));
+      do { // nothing
+      } while (TCA0.SINGLE.CNT < moment);
+      pin_off(&(PINS[pulse_widths[i].id]));
+      if (i == (N_LEDS - 1)) {
+        break;
+      }
+      ++i;
+    } while (1);
 
     // Update cycle count & reset if we've finished a set:
-    if (PERIOD_CYCLES == ++cycle_count) {
+    if (PERIOD_IN_CYCLES == ++cycle_count) {
       cycle_count = 0;
     }
 
-    // Update servo positions:
-    update_positions();
-
-    // Wait for the next cycle:
-    do {
-    } while (clock() > moment);
+    // Recalculate luminosities for the next cycle:
+    recalculate_pulse_widths();
 
     // And start over!
   } while (1);
